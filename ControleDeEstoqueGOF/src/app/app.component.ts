33a64fc3694fd108f4ed678518c8cd6bab5e11ff
Gabriel Masi <masigabriel9@gmail.com>
2025-04-03 20:44:01 -0300

    feature: header and sidemenu

--- ControleDeEstoqueGOF/src/app/app.component.ts
+++ ControleDeEstoqueGOF/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './pages/sidebar/sidebar.component';
+import { HeaderComponent } from './pages/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- ControleDeEstoqueGOF/src/app/app.routes.ts
+++ ControleDeEstoqueGOF/src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [{ path: 'login', component: LoginComponent }];
+export const routes: Routes = [
+    {path:'', component: LoginComponent},
+    {path:'login', component: LoginComponent},
+    {path:'home',component: HomeComponent}
+
+];
 
 export default routes;
