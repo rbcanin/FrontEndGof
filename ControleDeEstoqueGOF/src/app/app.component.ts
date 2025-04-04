ba0a6c0db2bb040b6a4644da32f8dbccbb55d2c9
Gabriel Masi <masigabriel9@gmail.com>
2025-04-03 21:43:43 -0300

    ajuste

--- ControleDeEstoqueGOF/src/app/app.component.ts
+++ ControleDeEstoqueGOF/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SidebarComponent } from './pages/sidebar/sidebar.component';
-import { HeaderComponent } from './pages/header/header.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarComponent, HeaderComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- ControleDeEstoqueGOF/src/app/pages/home/home.component.ts
+++ ControleDeEstoqueGOF/src/app/pages/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [SidebarComponent, HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
